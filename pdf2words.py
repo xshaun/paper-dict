75e62c92d19f4df8ff631fa8172547141519883c
SXY <xshaun@outlook.com>
2016-12-28 09:46:30 +0800

    indent using 4 spaces and resolve a warning issue

--- pdf2words.py
+++ pdf2words.py
@@ -1,52 +1,75 @@
 #!/usr/bin/python3.4
 # -*- coding: utf-8 -*-
 
-from pdfminer.pdfinterp import PDFResourceManager, process_pdf
+from io import StringIO
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
-from urllib.request import urlopen
+from pdfminer.pdfinterp import PDFResourceManager, process_pdf
 from pyquery import PyQuery as pyq
-from io import StringIO
-import re, os, sys, getopt
+from urllib.request import urlopen
+import getopt
+import logging 
+import os
+import re
+import sys
+
+
+# DETAILS: resolve an issue (get warnings while running)
+# 
+# Pdfminer3k logs to the Python root logger unfortunately.
+# It sets the root logger to level Error. 
+#   This will stop PDFMiner warn logging, 
+#   since it logs to the root logger, but not your own logging.
+#   
+# It sets propagation to False.
+#   Because after PDFMiner usage, I had duplicate logging entries. 
+#   This was caused by the root logger.
+
+logging.propagate = False 
+logging.getLogger().setLevel(logging.ERROR)
+
 
 def pdf2text(pdf_file):
-  rsrcmgr = PDFResourceManager()
-  retstr = StringIO()
-  laparams = LAParams()
-  device = TextConverter(rsrcmgr, retstr, laparams=laparams)
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, laparams=laparams)
 
-  process_pdf(rsrcmgr, device, pdf_file)
-  device.close()
+    process_pdf(rsrcmgr, device, pdf_file)
+    device.close()
 
-  content = retstr.getvalue()
-  retstr.close()
-  return (content)
+    content = retstr.getvalue()
+    retstr.close()
+    return (content)
+
 
 def text2words(text_string):
-  # modify broken words.
-  temp, number = re.compile(r'-\s{1}').subn('', text_string) 
+    # rectify words having a break.
+    temp, number = re.compile(r'-\s{1}').subn('', text_string) 
+    
+    # convert chinese characters into english characters.
+    temp, number = re.compile(r'’').subn('\'', temp)
+    
+    # eliminate characters not consisting in words.
+    temp, number = re.compile(r'[^a-zA-Z0-9\'-]').subn(' ', temp)
+    
+    # convert [blank,\t,\r,\n,\f,\v] into blank.
+    temp, number = re.compile(r'\s+').subn(' ', temp)
+        
+    # process words individually and remove redundancy by set.
+    rset = set()
+    for word in temp.split(' '):
+        # eliminate special chatacters at either end
+        word = word.strip('-').strip('\'')
 
-  # convert chinese characters into english characters.
-  temp, number = re.compile(r'’').subn('\'', temp)
+        # eliminate pure number/single-character/words-over-20-characters
+        if not re.compile(r'(^[0-9-]*$)|^.{1}$|^.{20,}$').match(word):
+            rset.add(word)
 
-  # eliminate characters which do not consist in words.
-  temp, number = re.compile(r'[^a-zA-Z0-9\'-]').subn(' ', temp)
+    result = list(rset)
+    result.sort()
+    return (result)
 
-  # convert [blank,\t,\r,\n,\f,\v] into blank.
-  temp, number = re.compile(r'\s+').subn(' ', temp)
-  
-  # process words individually and eliminate duplicates using set.
-  rset = set()
-  for word in temp.split(' '):
-    # eliminate special chatacters located at either end
-    word = word.strip('-').strip('\'')
-    # eliminate pure number/single character/words over 20 characters
-    if not re.compile(r'(^[0-9-]*$)|^.{1}$|^.{20,}$').match(word):
-      rset.add(word.lower())
-  
-  result = list(rset)
-  result.sort()
-  return (result)
 
 # structure of return value
 # [
@@ -56,83 +79,85 @@ def text2words(text_string):
 #   ...
 # ]
 def consult_bing(words_list):
-  result = []
-  rset = set()
-    
-  for word in words_list:
-    # response
-    while True:
-      try:
-        doc = pyq(url=r'http://cn.bing.com/dict/search?q=' + word)
-        break
-      except:
-        time.sleep(2)
-        continue
+    result = []
+    rset = set()
 
-    # DOM node of main content
-    cts = doc('.content>.rs_area>.lf_area>.qdef')
-    if not cts: continue
+    for word in words_list:
+        # HTML response
+        while True:
+            try:
+                doc = pyq(url=r'http://cn.bing.com/dict/search?q=' + word)
+                break
+            except:
+                time.sleep(2)
+                continue
 
-    # DOM node of searching word
-    keyword = cts.find('.hd_area>#headword>h1>strong').text()
-    if keyword in rset: continue
-   
-    # DOM node of word's pronunciation
-    pronunciation = cts.find('.hd_area>.hd_tf_lh>.hd_p1_1').text()
-    
-    # DOM node of word's explanation
-    explanation = []
-    for e in cts.find('ul').children():
-      e_pos = pyq(e).children('span.pos').text()
-      e_def = pyq(e).children('span.def').text()
-      explanation.append('[' + e_pos + ']' + e_def)
+        # DOM node of effective content
+        cts = doc('.content>.rs_area>.lf_area>.qdef')
+        if not cts: continue
 
-    rset.add(keyword)
-    word_info = [ x for x in [keyword, pronunciation, explanation] if x ]
-    #print (word_info)
-    result.append(word_info)
+        # DOM node of searching word
+        keyword = cts.find('.hd_area>#headword>h1>strong').text()
+        if keyword in rset: continue
 
-  result.sort()
-  return (result)
+        # DOM node of word's pronunciation
+        pronunciation = cts.find('.hd_area>.hd_tf_lh>.hd_p1_1').text()
+
+        # DOM node of word's explanation
+        explanation = []
+        for e in cts.find('ul').children():
+            e_pos = pyq(e).children('span.pos').text()
+            e_def = pyq(e).children('span.def').text()
+            explanation.append('[' + e_pos + ']' + e_def)
+
+        rset.add(keyword)
+        word_info = [ x for x in [keyword, pronunciation, explanation] if x ]
+        result.append(word_info)
+
+    result.sort()
+    return (result)
+
 
 def main(argv):
-  # argv
-  input_pdf_file = ''
-  try:
-    opts, args = getopt.getopt(argv,"hi:",["ifile="])
-  except getopt.GetoptError:
-    print ('pdf2words.py -i <input_pdf_file>')
-    sys.exit(2)
-  for opt, arg in opts:
-    if opt == '-h':
-       print ('pdf2words.py -i <input_pdf_file>')
-       sys.exit()
-    elif opt in ("-i", "--ifile"):
-       input_pdf_file = arg
-  
-  # if input is valid
-  try:
-    pdf_file = urlopen(input_pdf_file)
-  except:
+    # argv
+    input_pdf_file = ''
     try:
-      location = 'file://' + os.path.abspath(input_pdf_file)
-      pdf_file = urlopen(location)
-    except:
-      print (' invalid argv \'input_pdf_file\' ')
-      sys.exit()
+        opts, args = getopt.getopt(argv,"hi:",["ifile="])
+    except getopt.GetoptError:
+        print ('pdf2words.py -i <input_pdf_file>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('pdf2words.py -i <input_pdf_file>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            input_pdf_file = arg
 
-  # process
-  text_string = pdf2text(pdf_file)
-  words_list = text2words(text_string)
-  #print (words_list)
-  result = consult_bing(words_list)
+    # validity of input
+    try:
+        pdf_file = urlopen(input_pdf_file)
+    except:
+        try:
+            location = os.path.abspath(input_pdf_file)
+            pdf_file = open(location, 'rb')
+        except:
+            print (' invalid argv \'input_pdf_file\' ')
+            sys.exit()
+
+    # process
+    text_string = pdf2text(pdf_file)
+    words_list = text2words(text_string)
+    print (words_list)
+    result = consult_bing(words_list)
   
-  # show
-  print ('sum: ', len(result))
-  for record in result:
-    for item in record:
-      print (item)
+    # show
+    print ('sum words: ', len(result))
     print ('---------------')
+    for record in result:
+        for item in record:
+            print (item)
+            print ('---------------')
+
 
 if __name__ == "__main__":
-  main(sys.argv[1:])
+    main(sys.argv[1:])
